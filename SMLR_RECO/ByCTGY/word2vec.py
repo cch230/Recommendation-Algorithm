8131bb4d46ef687a03b3f784d996348899844411
cch230 <cch230@naver.com>
2023-09-27 11:41:59 +0900

    Implement word2vec
    Implement word2vec

---
+++ SMLR_RECO/ByCTGY/word2vec.py
@@ -0,0 +1,25 @@
+import pickle
+import sqlite3
+from typing import Optional
+
+from numpy import array
+from numpy.linalg import norm
+
+
+def similarity(word1: str, word2: str) -> float:
+    return cosine_similarity(get_word_vec(word1), get_word_vec(word2))
+
+
+def get_word_vec(word: str) -> Optional[array]:
+    cur = sqlite3.connect('../DataSet/CTGY_guesses.db').cursor()
+    cur.execute('SELECT vec FROM guesses WHERE word == ?', (word,))
+    if (fetched := cur.fetchone()) is not None:
+        return pickle.loads(fetched[1])
+    else:
+        raise KeyError(f'word {word} not found in DB')
+
+
+def cosine_similarity(vec1: array, vec2: array) -> float:
+    return vec1.dot(vec2) / (norm(vec1) * norm(vec2))
+
+print(similarity("자습서"))
